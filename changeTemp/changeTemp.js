5203b5ba451728c7f4f0176722e6fa1bf09c4355
Robby <rbpark@ucdavis.edu>
2019-04-18 02:57:26 -0700

    reorg files small code clean up

--- changeTemp/changeTemp.js
+++ changeTemp/changeTemp.js
@@ -1,8 +1,8 @@
 function change () {
 
 	let city = document.getElementById("city");
-	city.textContent = "Auburn"
+	city.textContent = "Auburn";
 
 	let icon = document.getElementById("iconSVG");
-	icon.src = "../../weather/weatherDesign/assets/fewclouds-day.svg"
+	icon.src = "../assets/fewclouds-day.svg";
 }
