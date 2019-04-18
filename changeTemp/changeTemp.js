cc762c36cca52100bedd1ee7f35bb372e06d238e
Robby <rbpark@ucdavis.edu>
2019-04-18 02:50:50 -0700

    added starter files

---
+++ changeTemp/changeTemp.js
@@ -0,0 +1,8 @@
+function change () {
+
+	let city = document.getElementById("city");
+	city.textContent = "Auburn"
+
+	let icon = document.getElementById("iconSVG");
+	icon.src = "../../weather/weatherDesign/assets/fewclouds-day.svg"
+}
